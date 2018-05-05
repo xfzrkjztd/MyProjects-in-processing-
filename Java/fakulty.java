0a94d4a061272b32d1cd50bbc1bf062f9264f55d
xfzrkjztd <kleinema@gmx.net>
2018-05-05 15:38:39 +0200

    Create fakulty.java

---
+++ Java/fakulty.java
@@ -0,0 +1,14 @@
+
+public class fakulty
+{       
+    
+    public int calc(int n){
+        int sum = 1;
+        while(n != 1){
+           sum *= n;
+           n--;
+            
+        }
+        return sum;
+    }
+}
